be6465c37e8c26676a97d9af7edc657637bcc55e
Lucas Pedrone <lucaspedrone@gmail.com>
2021-01-14 11:41:00 -0300

    Implementações referentes a Sessão 3 do curso de Spring: Spring Context, Bean Life Cycle, Dependency Injection, Profiles

---
+++ src/test/java/guru/springframework/sfgdi/controllers/ConstructorInjectedControllerTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgdi.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
+
+public class ConstructorInjectedControllerTest {
+	
+    ConstructorInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new ConstructorInjectedController(new ConstructorGreetingService());
+    }
+
+    @Test
+    void getGreeting() {
+
+        System.out.println(controller.getGreeting());
+
+    }
+
+}
